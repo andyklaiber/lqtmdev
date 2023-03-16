35fc908be3e565618ae673fd0e70112c2264f448
Andy Klaiber <andy.klaiber@gmail.com>
2023-03-15 21:36:08 -0700

    fix: got first api test to work

--- test/api/auth/session.test.js
+++ test/api/auth/session.test.js
@@ -1,14 +1,23 @@
 'use strict'
-
+const path = require('path');
+require('dotenv').config({path:path.resolve(process.cwd(), 'test/api/helpers/.env.test')})
 const { test } = require('tap')
-const build = require('../../../app')
+const fastify = require('fastify');
+const appModules = require('../../../app')
+
 
 test('requests the "/" route', async t => {
-  const app = build()
+  const app = fastify()
+  app.register(appModules);
+  t.teardown(() => app.close()) 
 
   const response = await app.inject({
     method: 'POST',
-    url: '/api/auth/login'
+    url: '/api/auth/login',
+    payload:{
+      email:"goober",
+      password:"bar"
+    }
   })
-  t.equal(response.statusCode, 200, 'returns a status code of 200')
+  t.equal(response.statusCode, 401, 'returns a status code of 401 with bad creds')
 })
